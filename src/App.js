d786e899364017f0fedb740d5340b06c2fde3855
Nemanja Kuzmanovic <kuzmaneca@gmail.com>
2020-03-08 12:44:17 +0100

    broken homepage into smaller components, finished styling menu-item component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Homepage from './homepage.components';
+import Homepage from './pages/homepage/homepage.components';
 
 const App = () => {
   return (
